d608cb48545c1042fb000c0d098b184fa93a6d39
Infectedd <b.slavchev@student.maastrichtuniversity.nl>
2018-02-28 16:19:00 +0100

    update

--- core/src/com/mygdx/sim/GameObjects/Car.java
+++ core/src/com/mygdx/sim/GameObjects/Car.java
@@ -2,18 +2,27 @@ package com.mygdx.sim.GameObjects;
 
 public class Car extends Vehicle {
 
+	Edge locationEdge;
+	double distanceTraveledOnEdge;
+
 	public Car(Node startNode, Node goalNode, int maxSpeed) {
 		super(startNode, goalNode);
-		
+
 		// Set Max Speed
 		this.maxSpeed = maxSpeed;
-				
+
 		// Set Sprite Name
 		spriteName = "car";
 	}
 
-	// This function can be used as a multiplier to the car's speed so that he is
-	// willing to go X percent faster than the speed limit. Not 100% necessary.
+	/**
+	 *  This function can be used as a multiplier to the car's speed so that he is
+	 *  willing to go X percent faster than the speed limit. Not 100% necessary.
+	 * @param timeLimit
+	 * @param from
+	 * @param to
+	 * @return
+	 */
 	public double setHasteValue(double timeLimit, Node from, Node to) {
 		double eDistance = Math.abs(Math.sqrt((to.getX() - from.getX()) + to.getY() - from.getY()));
 		return eDistance / timeLimit;
