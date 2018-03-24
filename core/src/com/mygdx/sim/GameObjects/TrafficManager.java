675fe5816a4e63235c7c6a017eb1d77880ef6f20
Infectedd <b.slavchev@student.maastrichtuniversity.nl>
2018-03-24 16:59:34 +0100

    readded simple testcase

--- core/src/com/mygdx/sim/GameObjects/TrafficManager.java
+++ core/src/com/mygdx/sim/GameObjects/TrafficManager.java
@@ -225,6 +225,35 @@ public class TrafficManager {
 		
 		int x =0;
 	}
+	
+	public static TrafficManager createSimpleTestcase() {
+		Node node1 = new Node(0,0);
+		Node node2 = new Node(475,0);
+		Node node3 = new Node(475,1000);
+		Edge edge1 = new Edge(node1,node2);
+		Edge edge2 = new Edge(node2,node3);
+		
+		Map map = new Map(Arrays.asList(node1,node2,node3),Arrays.asList(edge1,edge2));
+		
+		Car car1 = new Car(node2,node3,map);
+		car1.setEdgePath(Arrays.asList(edge2));
+		car1.setDriverModel(new SimpleDriverModel(10));
+		
+		Car car2 = new Car(node2,node3,map);
+		car2.setEdgePath(Arrays.asList(edge2));
+		car2.setDriverModel(new IntelligentDriverModel());
+		
+		Car car3 = new Car(node1,node3,map);
+		car3.setEdgePath(Arrays.asList(edge1,edge2));
+		car3.setDriverModel(new IntelligentDriverModel());
+		
+		List cars = Arrays.asList(car1,car2,car3);
+		
+		TrafficManager tm = new TrafficManager(map,cars);
+		int y= 0;
+		
+		return tm;
+	}
 
 	public static TrafficManager createEnvironment() {
 
