78f843ed1a8fedafba8194a5c76afbb9bd1db448
Infectedd <b.slavchev@student.maastrichtuniversity.nl>
2018-03-24 17:04:41 +0100

    minor cleanup

--- core/src/com/mygdx/sim/GameObjects/TrafficManager.java
+++ core/src/com/mygdx/sim/GameObjects/TrafficManager.java
@@ -217,16 +217,6 @@ public class TrafficManager {
 		return Math.abs(Math.sqrt(Math.pow((a.getY()-b.getY()), 2) + Math.pow((a.getX() - b.getX()), 2)));
 	}
 	
-	public static void main(String[] args) {
-		TrafficManager tm = createEnvironment();
-		
-		tm.simulate(TIMESTEPS);
-		
-		System.out.println("Simulation completed");
-		
-		int x =0;
-	}
-	
 	public static TrafficManager createSimpleTestcase() {
 		Node node1 = new Node(0,0);
 		Node node2 = new Node(475,0);
@@ -323,41 +313,13 @@ public class TrafficManager {
 		return "[TrafficManager]";
 	}
 
-	/*
 	public static void main(String[] args) {
-		Node node1 = new Node(0,0);
-		Node node2 = new Node(475,0);
-		Node node3 = new Node(475,1000);
-		Edge edge1 = new Edge(node1,node2);
-		Edge edge2 = new Edge(node2,node3);
-		
-		Map map = new Map(Arrays.asList(node1,node2,node3),Arrays.asList(edge1,edge2));
-
-
-		Car car1 = new Car(node2,node3,map);
-		// car1.setEdgePath(Arrays.asList(edge2));
-		// car1.computePath(0);
-		car1.setDriverModel(new SimpleDriverModel(10));
-
-		Car car2 = new Car(node2,node3,map);
-		// car2.setEdgePath(Arrays.asList(edge2));
-		// car2.computePath(0);
-		car2.setDriverModel(new IntelligentDriverModel());
-
-
-		Car car3 = new Car(node1,node3,map);
-		// car3.setEdgePath(Arrays.asList(edge1,edge2));
-		// car3.computePath(0);
-		car3.setDriverModel(new IntelligentDriverModel());
-		
-		List cars = Arrays.asList(car1,car2,car3);
-		
-		TrafficManager tm = new TrafficManager(map,cars);
+		TrafficManager tm = createSimpleTestcase();
 		
 		tm.simulate(TIMESTEPS);
 		
-		int y = 0;
-
+		System.out.println("Simulation completed");
+		
+		int x =0;
 	}
-	*/
 }
