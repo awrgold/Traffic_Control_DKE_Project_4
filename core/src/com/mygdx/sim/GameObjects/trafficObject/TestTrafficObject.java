6564606214ed908853ab3ef1b077cf8b2c5b525b
Infectedd <b.slavchev@student.maastrichtuniversity.nl>
2018-06-21 11:52:22 +0200

    testcase

---
+++ core/src/com/mygdx/sim/GameObjects/trafficObject/TestTrafficObject.java
@@ -0,0 +1,19 @@
+package com.mygdx.sim.GameObjects.trafficObject;
+
+import com.mygdx.sim.GameObjects.data.Location;
+
+public class TestTrafficObject implements TrafficObject {
+	
+	private Location location;
+	
+	public TestTrafficObject(Location loc) {
+		this.location = loc;
+	}
+
+	@Override
+	public TrafficObjectState getState(int timestep) {
+		return new TrafficObjectState(null, location, 0, false, true);
+	}
+	
+
+}
